34b9e6ebeaaca4a43ae409428ac166735cba8dd8
adbzxcghf <131544433+adbzxcghf@users.noreply.github.com>
2024-01-01 02:21:48 +0800

    Add files via upload

--- fs/exec.c
+++ fs/exec.c
@@ -1863,7 +1863,6 @@ extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *ar
 			void *envp, int *flags);
 extern int ksu_handle_execveat_sucompat(int *fd, struct filename **filename_ptr,
 				 void *argv, void *envp, int *flags);
-
 static int do_execveat_common(int fd, struct filename *filename,
 			      struct user_arg_ptr argv,
 			      struct user_arg_ptr envp,
@@ -1873,7 +1872,7 @@ static int do_execveat_common(int fd, struct filename *filename,
 	if (unlikely(ksu_execveat_hook))
 		ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
 	else
-		ksu_handle_execveat_sucompat(&fd, &filename, &argv, &envp, &flags);
+		ksu_handle_execveat_sucompat(&fd, &filename, &argv, &envp      &flags);
 	return __do_execve_file(fd, filename, argv, envp, flags, NULL);
 }
 
